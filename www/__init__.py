d30cb40f674dac2794fe65a1fa062a22274f1d85
Ilya Zverev <ilya@zverev.info>
2025-05-15 17:43:46 +0300

    proxyfix

--- www/__init__.py
+++ www/__init__.py
@@ -1,7 +1,10 @@
 from flask import Flask
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = Flask(__name__)
 app.config.from_object('config')
+app.wsgi_app = ProxyFix(
+    app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
 
 try:
     from flask_compress import Compress
